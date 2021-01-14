f0dd99bd01d7a96914f3498382786ef48f4f2110
harrysingh2k19 <49354764+harrysingh2k19@users.noreply.github.com>
2021-01-14 18:36:52 +0530

    code with comment

--- RealStateTickets/Tickets/views.py
+++ RealStateTickets/Tickets/views.py
@@ -10,15 +10,15 @@ from django.http import HttpResponse
 import datetime
 
 def index(request):
-    if request.user.is_authenticated :
+    if request.user.is_authenticated :# To Check user logged redirect to dashboard
         return redirect('dashboard')
     else:
-        if request.method == 'GET':
+        if request.method == 'GET': #  if it is not POST some data then it want data GET
             return render(request, 'signin.html',{'user_form': AuthenticationForm(),'login':True})
-        else:
+        else:  # If request id POST
             user_input = form_user(data=request.POST)
             user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-            if user is None:
+            if user is None: # if not authenticated
                 return render(request, 'signin.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
             else:
                 login(request, user)
@@ -33,7 +33,7 @@ def signup(request):
         if user_input.is_valid():
             user = user_input.save()
             temp=user_input.cleaned_data['email']
-            user.username = temp
+            user.username = temp                     # to allocated the email id as username
             user.set_password(user.password)
             user.save()
             return redirect("index")
@@ -41,7 +41,7 @@ def signup(request):
             return redirect("index")
 
 
-def new_request(request):
+def new_request(request):  # to raise new ticket
     if request.user.is_authenticated :
         if request.method == 'GET':
             user_form = form_info
@@ -52,58 +52,53 @@ def new_request(request):
                 user = user_input.save(commit=False)
                 user.user = request.user
                 user.save()
-                return redirect("dashboard")
+                return redirect("dashboard") # after save data return dashboard
             else:
+                print(user_input.errors)        # If got anny error
                 return HttpResponseRedirect('/')
-    else:
+    else: # if user not logged
         return redirect("dashboard")
 
 def dashboard(request):
     if request.user.is_authenticated :
-        if model_admin.objects.filter(user=request.user): #Admin
-            user_data = model_info.objects.all()
+        if model_admin.objects.filter(user=request.user):   # Admin
+            user_data = model_info.objects.exclude(request_status = 'Complete')    #print all entry except with complete status
             return render(request, 'dashboard.html',{'user_data':user_data, 'admin':True})
-        else:# for genral user
-            user_data = model_info.objects.filter(user=request.user)
+        else:                                               # for genral user
+            user_data = model_info.objects.filter(user=request.user) # all data of raised by the user
             if len(user_data)==0:
                 return render(request, 'dashboard.html',{'user_data':False})
             else:
                 return render(request, 'dashboard.html',{'user_data':user_data})
-
     else:
-        return redirect("index")
+        return redirect("index") # if user not logged
 
 def details(request, pk):
-    if request.method == 'GET':
+    if request.method == 'GET':                    # if page not sending any data
         if model_info.objects.all().filter(id =pk ):# For Admin
             if model_admin.objects.filter(user=request.user):
                 article = model_info.objects.all().filter(id =pk )
                 return render(request, 'details.html',{'user_data':article, 'admin':True,'user_form':form_details})
-            else: # user
+            else:                                   # user
                 article = model_info.objects.all().filter(id =pk )
                 article2 = model_assign.objects.all().filter(id =pk )
                 return render(request, 'details.html',{'user_data':article, 'admin':False, 'user_dataa':article2})
         else:
             return render(request, 'dashboard.html',{'user_data':False, 'admin':True})
-    else :
+    else :                                                    # post
         user_input = form_details(data=request.POST)
         if user_input.is_valid():
             article = model_info.objects.all().filter(id =pk )
-            for a in article:
+            for a in article:  # save valuse in model_info
                 a.request_status = user_input.cleaned_data['request_status']
                 a.request_remark = user_input.cleaned_data['request_remark']
-                for b in model_admin.objects.filter(user=request.user):
-                    a.request_assigned = b
+                for b in model_admin.objects.filter(user=request.user): a.request_assigned = b
                 a.save()
             return redirect("dashboard")
         else:
             return redirect("dashboard")
 
-
-
-
-
-def signout(request):
+def signout(request):   # for signout
     if request.user.is_authenticated:
         logout(request)
         return HttpResponseRedirect('/')
